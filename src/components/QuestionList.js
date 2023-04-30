85576d05419fa0bf666c4c514819d8f0b34fd888
Wahome <vinwahome@gmail.com>
2023-04-30 16:17:29 +0300

    Assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,24 @@
+import { useState, useEffect } from "react";
 import React from "react";
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+      .then(response => response.json())
+      .then(data => setQuestions(data))
+      .catch(error => console.error(error));
+  }, []);
+  
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{/* display QuestionItem components here after fetching */}
+        <QuestionList questions={questions} />
+      </ul>
     </section>
   );
 }
